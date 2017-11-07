3245134796d0b6c53ffefbc6548e9f19a389b72c
xu <774590465@qq.com>
2017-11-07 20:59:48 +0800

    20171107
    netty客户端和服务端实例

---
+++ netty/core/ClientHandler.java
@@ -0,0 +1,17 @@
+package netty.core;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("client接收到服务器返回的消息: " + msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("client exception is general ");
+    }
+    
+}
