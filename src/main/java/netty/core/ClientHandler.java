b243149344f79d6eb3fcde995c635e95416cf273
youmeimao <zhangzhiyuan218@gmail.com>
2017-11-08 12:47:48 +0800

    modify pom

--- src/main/java/netty/core/ClientHandler.java
+++ src/main/java/netty/core/ClientHandler.java
@@ -6,7 +6,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class ClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("client���յ����������ص���Ϣ: " + msg);
+        System.out.println("client read messge: " + msg);
     }
 
     @Override
